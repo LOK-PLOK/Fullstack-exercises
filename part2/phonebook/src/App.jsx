4a9393fd63357105232bbc5043e12e4653ea9951
PaulFranceM.Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2024-08-29 02:20:18 +0800

    phoneBook finished

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Filter from './components/Filter'
 const App = () => {
-  const [persons, setPersons] = useState([  //needs unique ID
+  const [persons, setPersons] = useState([ 
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
@@ -18,6 +19,13 @@ const App = () => {
     setNewNumber('')
   }
 
+  const newSet = (nameObject) =>{
+    setPersons(persons.concat(nameObject))
+    setSearchResult(persons.concat(nameObject))
+    console.log(persons)
+    console.log(searchResult)
+  }
+
   const handleAddContact = (event) =>{ //Too cluttered
     event.preventDefault()
     const lastID = persons.length
@@ -26,15 +34,20 @@ const App = () => {
     if(check){
       alert(`${newName} is already added to phonebook`)
     }else{
-      setPersons(persons.concat(nameObject))
-      setSearchResult(persons.concat(nameObject))
+      newSet(nameObject)
     }
     clearInputs()
   }
   
-  const find = (event) => {
+  const find = (event) => {// make the code neet
     console.log(event.target.value)
-    
+    const search = event.target.value
+    console.log(search)
+    setSearchResult(
+      search === '' 
+        ? persons 
+        : persons.filter(person => person.name.includes(search))
+    )
   }
   
   const handleNameChange = (event)=>{
@@ -55,11 +68,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-        {/* Search needs improvements */}
-      <form> 
-        <div>filter shown with</div>
-        <input value={search} onChange={handleNewSearch}/>
-      </form>
+     
+      <Filter find = {find} value ={search} handleNewSearch = {handleNewSearch}/>
 
       <h2>add a new</h2>
         <PersonForm handleAddContact = {handleAddContact}
--- part2/phonebook/src/components/Filter.jsx
+++ part2/phonebook/src/components/Filter.jsx
@@ -1,1 +1,10 @@
-const Filter = ({})
+const Filter = ({find,search,handleNewSearch}) =>{
+      return(
+      <form onChange={find}> 
+        <div>filter shown with</div>
+        <input value={search} onChange={handleNewSearch}/>
+      </form>
+      )
+}
+
+export default Filter
