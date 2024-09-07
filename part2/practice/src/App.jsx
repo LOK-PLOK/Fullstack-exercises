ec1ce9495db56ed11efe7a22ff0d441f3450dc56
PaulFranceM.Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2024-09-07 14:21:07 +0800

    JSON server and useEffect application

--- part2/practice/src/App.jsx
+++ part2/practice/src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('') 
   const [showAll,setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  
+  useEffect(hook, [])
+  
+
+  console.log('render',notes.length,'notes')
+  
   
   const addNote = (event) => {
     event.preventDefault()
@@ -27,8 +44,7 @@ const App = (props) => {
   ? notes
   : notes.filter(note => note.important === true)
 
-  console.log(notes)
-  console.log(notesToShow)
+
   return (
     <div>
       <h1>Notes</h1>
